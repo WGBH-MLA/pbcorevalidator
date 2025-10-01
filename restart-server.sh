a56549fcd4a6ae1c2effcc4cdd6f4fc6c16a804c
fog <foglaboratories@gmail.com>
2025-10-01 14:51:17 -0400

    'add rvm thingy'

--- restart-server.sh
+++ restart-server.sh
@@ -4,7 +4,6 @@ if [[ $(/usr/sbin/lsof -ti :4567) ]]; then
     echo "Server already running... Cool!"
 else
     echo "Starting reg Server"
-    # cd /var/app/album-timeline && . ~/.nvm/nvm.sh && nohup node backend.js 0<&- &>/home/ec2-user/boot-log.log &
-    cd /var/app/pbcorevalidator && RAILS_ENV=production nohup bundle exec ruby sinatra-dispatch.rb 0<&- &>>/home/ec2-user/boot-log.log &
+    cd /var/app/pbcorevalidator && [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" && rvm use 3.1.4 && RAILS_ENV=production nohup bundle exec ruby sinatra-dispatch.rb 0<&- &>>/home/ec2-user/boot-log.log &
     echo "I rly deed it"
 fi
